6eaacf18a368acba379eb7646abaa0ad04411046
wistic <prathameshkatkar11@gmail.com>
2020-10-19 22:24:09 +0530

    Project setup

---
+++ flask-backend/static/react/precache-manifest.4ddf5ba507f7920a78cacdbdf9b8b88f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681cb98c5b8f937ad59f00c1be5473b9",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "933f747693ac9e9a4624",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82c43f5565cb2977d8fb",
+    "url": "/static/react/js/2.a09e5cb0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/react/js/2.a09e5cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933f747693ac9e9a4624",
+    "url": "/static/react/js/main.e6b51b67.chunk.js"
+  },
+  {
+    "revision": "642bffd405cc52de04d1",
+    "url": "/static/react/js/runtime-main.8fa3ef7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
